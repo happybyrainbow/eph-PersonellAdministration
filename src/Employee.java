5acebf80882d3948c06b041182a4b8a9a1b59608
turimaks <maximturianskyi@gmail.com>
2022-09-06 11:28:40 +0200

    Anfang

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,27 @@
 public class Employee {
+
+    private String name;
+    private String surname;
+    private String birthday;
+    private char gender;
+    private int age;
+    private double weight;
+    private String department;
+    private int salary;
+    private int yearsOfService;
+    private String specialties;
+
+    public Employee(String name, String surname, String birthday, char gender, int age, double weight, String department, int salary, int yearsOfService, String specialties){
+        this.name = name;
+        this.surname = surname;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.age;
+        this.weight;
+        this.department;
+        this.salary;
+        this.yearsOfService;
+        this.specialties;
+    }
+
 }
