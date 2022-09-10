ac90521d50f001630867d5990c04065f533b0290
happybyrainbow <maximturianskyi@gmail.com>
2022-09-10 21:49:29 +0200

    Anfang

--- src/Employee.java
+++ src/Employee.java
@@ -1,5 +1,4 @@
 public class Employee {
-
     private String name;
     private String surname;
     private String birthday;
@@ -11,17 +10,79 @@ public class Employee {
     private int yearsOfService;
     private String specialties;
 
+
     public Employee(String name, String surname, String birthday, char gender, int age, double weight, String department, int salary, int yearsOfService, String specialties){
         this.name = name;
         this.surname = surname;
         this.birthday = birthday;
         this.gender = gender;
-        this.age;
-        this.weight;
-        this.department;
-        this.salary;
-        this.yearsOfService;
-        this.specialties;
+        this.age = age;
+        this.weight = weight;
+        this.department = department;
+        this.salary = salary;
+        this.yearsOfService = yearsOfService;
+        this.specialties = specialties;
+
+    }
+
+    public String getName(){
+
+        return name;
+
+    }
+
+    public String getSurname(){
+
+        return surname;
+
+    }
+
+    public String getBirthday(){
+
+        return birthday;
+
+    }
+
+    public char getGender(){
+
+        return gender;
+
+    }
+
+    public int getAge(){
+
+        return age;
+
+    }
+
+    public double getWeight(){
+
+        return weight;
+
+    }
+
+    public String getDepartment(){
+
+        return department;
+
+    }
+
+    public int getSalary(){
+
+        return salary;
+
+    }
+
+    public int getYearsOfService(){
+
+        return yearsOfService;
+
+    }
+
+    public String getSpecialties(){
+
+        return specialties;
+
     }
 
 }
--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,2 +1,15 @@
+
+
 public class MainProgram {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Maksym", "Turianskyi", "2007.04.23", 'r', 61, 58.4, "C2", 1500, 0, "Verschwendet 4 Stunden seines Lebens für eine Schach Partie" );
+        System.out.println("Name: " + e1.getName() +  "\nNachname: " + e1.getSurname()  + "\nGeburtstag: " + e1.getBirthday() + "\nGeschlecht: " + e1.getGender() + "\nAlter: " + e1.getAge() + "\nGewicht: " + e1.getWeight() + "\nAbteilung: " + e1.getDepartment() + "\nGehalt: " + e1.getSalary() + "\nDienstjahre: " + e1.getYearsOfService() + "\nBesonderheiten: " + e1.getSpecialties());
+
+        if(e1.getAge()>60){
+            System.out.println("\nGeh in Rente");
+        }
+        if(e1.getGender() != 'm' && e1.getGender() != 'w' && e1.getGender() != 'd'){
+            System.out.println("Bist du ein Einhorn oder what");
+        }
+    }
 }
