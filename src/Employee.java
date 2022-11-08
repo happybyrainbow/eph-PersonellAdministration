fdd9b52e97c7bcc01391f7357d428617f2328efc
happybyrainbow <maximturianskyi@gmail.com>
2022-11-08 19:13:21 +0100

    lol

--- src/Employee.java
+++ src/Employee.java
@@ -9,9 +9,11 @@ public class Employee {
     private double salary;
     private int yearsOfService;
     private String specialties;
+    private int IQ;
+    private double number;
+    private int days;
 
-
-    public Employee(String name, String surname, String birthday, char gender, int age, double weight, String department, double salary, int yearsOfService, String specialties){
+    public Employee(String name, String surname, String birthday, char gender, int age, double weight, String department, double salary, int yearsOfService, String specialties, int IQ){
         this.name = name;
         this.surname = surname;
         this.birthday = birthday;
@@ -22,6 +24,7 @@ public class Employee {
         this.salary = salary;
         this.yearsOfService = yearsOfService;
         this.specialties = specialties;
+        this.IQ = IQ;
 
     }
 
@@ -85,6 +88,16 @@ public class Employee {
 
     }
 
+    public int getIQ(){
+
+        return IQ;
+
+    }
+
+    public String getPersonalInformation(String name, String surname, String birthday){
+        return name + surname + birthday;
+    }
+
     public void setName(String name){
 
         this.name = name;
@@ -120,4 +133,15 @@ public class Employee {
 
     }
 
+    public void setIQ(int IQ){
+
+        this.IQ = IQ;
+
+    }
+
+    public void HappyBirthday(){
+        if(days > 365){
+            System.out.println("Happy Birthday!!!!");
+        }
+    }
 }
